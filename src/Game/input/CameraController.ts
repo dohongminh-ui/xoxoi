210ca5560235133e6890bb67e209f8456c7ebe6f
hominhIsStupid <ngominh04112007@gmail.com>
2025-08-22 15:10:45 +0700

    Minor camera tweak

--- src/Game/input/CameraController.ts
+++ src/Game/input/CameraController.ts
@@ -562,12 +562,11 @@ export class CameraController extends EventTarget {
    resetCamera() {
       this.velocity = {x: 0, y: 0};
 
-      if (!GRID_SIZE.INFINITE_X && !GRID_SIZE.INFINITE_Y) {
-         const mid_x = GRID_SIZE.X / 2;
-         const mid_y = GRID_SIZE.Y / 2;
-
-         this.cameraToCell(mid_x, mid_y);
-      } else this.setCameraState(0, 0, 1);
+      let toX = 0;
+      let toY = 0;
+      if (!GRID_SIZE.INFINITE_X) toX = GRID_SIZE.X / 2;
+      if (!GRID_SIZE.INFINITE_Y) toY = GRID_SIZE.Y / 2;
+      this.cameraToCell(toX, toY);
 
       this.dispatchEvent(
          new CustomEvent('cameraReset', {
